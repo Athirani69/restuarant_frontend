ccca22ae5fe97c13a1698448b12fb26018c09369
Athirani69 <Athirani69@gmail.com>
2024-02-09 01:18:10 +0530

    new

---
+++ lib/Pages/view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class vew extends StatefulWidget {
+  const vew({super.key});
+
+  @override
+  State<vew> createState() => _vewState();
+}
+
+class _vewState extends State<vew> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+
+      ),
+    );
+  }
+}
